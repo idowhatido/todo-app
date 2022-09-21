43da59d4e6b2cd92cd1db1761d48ed772d730999
SachinSingh <Sachin.Singh1@ibm.com>
2022-09-21 14:35:03 +0530

    Added Mongo

--- app.js
+++ app.js
@@ -1,46 +1,135 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const date = require(__dirname + "/date.js");
 const app = express();
+const _ = require("lodash");
 
-const  itemlist = ["test1", "test2"];
-const workitem = [];
+// const  itemlist = ["test1", "test2"];
+// const workitem = [];
+
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 
+mongoose.connect("mongodb://localhost:27017/todolist", {useNewUrlParser : true})
+
+const itemSchema = {
+    name : String
+};
+
+const Item = mongoose.model("Item", itemSchema);
+
+const Item1 = new Item({
+    name : "Get Up"
+});
+
+const Item2 = new Item({
+    name : "Brush Teeth"
+});
+
+const defaultItems = [Item1, Item2];
+
+
+const listSchema = {
+    name : String,
+    items : [itemSchema]
+}
+
+const List = mongoose.model("List", listSchema);
 
 app.get("/", function (request, responce) {
 
     let day = date.getDate();
-    responce.render("list", {ListTitle : day, itemlist : itemlist});
+    
+    Item.find({}, function(err, items){
+
+        if(items.length ==0){
+            Item.insertMany(defaultItems, function(err){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    console.log("Default Added");
+                }
+            });
+            responce.redirect("/")
+        }
+        else{
+            responce.render("list", {ListTitle : "today", itemlist : items});
+        }
+        
+    });
 });
 
 app.post("/", function (request, responce){
     let item = request.body.listItem;
+    let listName = request.body.list; 
 
-    if(request.body.list == "Work"){
-        workitem.push(item);
-        responce.redirect("/work");
+    const newItem = new Item({
+        name : item
+    })
+
+    if(listName == "today"){
+        newItem.save();
+        responce.redirect("/");
     }
     else{
-        itemlist.push(item);
-        responce.redirect("/");
+        List.findOne({name : listName}, function(err, foundList){
+            foundList.items.push(newItem);
+            foundList.save();
+        });
+        responce.redirect("/" + listName);
     }
 });
 
-app.get("/work", function (request, responce) {
-    responce.render("list", {ListTitle: "Work", itemlist:workitem });
-})
-// app.post("/post", function(request, responce){
-//     let item = request.body.listItem;
-//     workitem.push(item);   
-//})
+app.post("/delete", function(request, responce){
+    const deleteItem = request.body.checkbox;
+    const listName = request.body.ListName;
 
-app.get("/about", function(request, responce){
-    responce.render("about")
+    if(listName == "today"){
+        console.log(deleteItem);
+        Item.findByIdAndRemove(deleteItem, function(err){
+            console.log(err);
+        })
+        responce.redirect("/");
+    }
+    else{
+        List.findOneAndUpdate({name:listName}, {$pull : {items: {_id : deleteItem}}}, function(err, foundList){
+            if(!err){
+                responce.redirect("/" + listName);
+            }
+        });
+    }
+});
+
+
+app.get("/:listName", function (request, responce) {
+    const listName = _.capitalize(request.params.listName);
+
+    List.findOne({name : listName}, function(err, foundList){
+        if(!err){
+            if(!foundList){
+                const list = new List({
+                    name : listName,
+                    items : defaultItems
+                });
+                list.save();
+                responce.redirect("/" + listName);
+            }
+            else{
+                responce.render("list", {ListTitle : listName, itemlist : foundList.items});
+            }
+        }
+    });
 })
+
+
+// app.get("/about", function(request, responce){
+//     responce.render("about")
+// })
 
 app.listen(3000, function(){
     console.log("server started");
